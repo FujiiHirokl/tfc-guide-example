71954083857d0b138c6151aaaf665d5d507f1489
hiroki3905 <96215671+hiroki3905@users.noreply.github.com>
2024-02-19 14:16:03 +0900

    test

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+
 provider "aws" {
-  region = var.region
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
 }
 
 data "aws_ami" "ubuntu" {
--- variables.tf
+++ variables.tf
@@ -1,9 +1,18 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "ap-northeast-1"
+}
+
+variable "secret_key" {
+  description = "AWS secret_key"
+  default     = ""
+}
+
+variable "access_key" {
+  description = "AWS access_key"
+  default     = ""
 }
 
 variable "instance_type" {
