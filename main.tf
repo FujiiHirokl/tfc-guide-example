b18bbde2eaded2eddb1c648cee670f5ffa9c46ff
hiroki3905 <96215671+hiroki3905@users.noreply.github.com>
2024-02-19 14:58:37 +0900

    vvv

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
-
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region     = "ap-northeast-1"
 }
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
--- variables.tf
+++ variables.tf
@@ -1,19 +1,5 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-variable "region" {
-  description = "AWS region"
-  default     = "ap-northeast-1"
-}
-
-variable "secret_key" {
-  description = "AWS secret_key"
-  default     = ""
-}
-
-variable "access_key" {
-  description = "AWS access_key"
-  default     = ""
-}
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
